fb49ef9ee014e75c7a78b6e2fe775673fcb3a9df
Makmiller <makmiller@gmail.com>
2014-08-27 13:57:15 -0400

    Ask before closing conkeror window

--- conkerorrc.js
+++ conkerorrc.js
@@ -535,6 +535,16 @@ define_key(content_buffer_normal_keymap, "H", "find-url-from-history-new-buffer"
 define_key(content_buffer_normal_keymap, "h", "find-url-from-history");
 
 
+add_hook("window_before_close_hook",
+         function () {
+             var w = get_recent_conkeror_window();
+             var result = (w == null) ||
+                 "y" == (yield w.minibuffer.read_single_character_option(
+                     $prompt = "Quit Conkeror? (y/n)",
+                     $options = ["y", "n"]));
+             yield co_return(result);
+         });
+
 dumpln("END OF RC FILE");
 
 // //command-line fu define_webjump("commandlinefu", function(term) {
