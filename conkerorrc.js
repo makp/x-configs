cf1f9af2231a1e6449c29526b45e1373d09e19c6
Makmiller <makmiller@gmail.com>
2015-02-15 11:26:43 -0500

    added password management conkeror

--- conkerorrc.js
+++ conkerorrc.js
@@ -306,13 +306,13 @@ function create_selection_search(webjump, key) {
                     return term;});
     define_key(content_buffer_normal_keymap, key, "prompted-" + webjump + "-search");
 
-    // webjump with selection (with upper case key)
-    interactive(webjump+"-selection-search",
-                "Search " + webjump + " with selection contents",
-                "find-url-new-buffer",
-                $browser_object = function (I) {
-                    return webjump + " " + I.buffer.top_frame.getSelection();});
-    define_key(content_buffer_normal_keymap, key.toUpperCase(), webjump + "-selection-search");
+    // // webjump with selection (with upper case key)
+    // interactive(webjump+"-selection-search",
+    //             "Search " + webjump + " with selection contents",
+    //             "find-url-new-buffer",
+    //             $browser_object = function (I) {
+    //                 return webjump + " " + I.buffer.top_frame.getSelection();});
+    // define_key(content_buffer_normal_keymap, key.toUpperCase(), webjump + "-selection-search");
 }
 
 create_selection_search("google","g");
@@ -436,10 +436,76 @@ $alternative="http://stackoverflow.com");
 // ========
 // Password
 // ========
+/*
+  Implements new login-manager.
+  See:
+  - http://librelist.com/browser//conkeror/2013/11/28/login-manager-xulrunner-24-0/
+  - https://blog.mozilla.org/dolske/2013/08/21/a-change-in-password-manager/
+
+  (C) Copyright 2014 thorkill
+  BSD License
+*/
+
+let Cu = Components.utils;
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+XPCOMUtils.defineLazyModuleGetter(this,
+                                  "LoginManagerContent",
+                                  "resource://gre/modules/LoginManagerContent.jsm");
+XPCOMUtils.defineLazyModuleGetter(this,
+                                  "LoginManagerParent",
+                                  "resource://gre/modules/LoginManagerParent.jsm");
+
+define_buffer_local_hook("content_buffer_dom_form_has_password_hook");
+define_buffer_local_hook("content_buffer_dom_auto_complete_hook");
+
+// passwords
 session_pref("signon.rememberSignons", true);
 session_pref("signon.expireMasterPassword", false);
 session_pref("signon.SignonFileName", "signons.txt");
-Cc["@mozilla.org/login-manager;1"].getService(Ci.nsILoginManager); // init
+session_pref("signon.useDOMFormHasPassword", true);
+
+Cc["@mozilla.org/login-manager;1"].getService(Ci.nsILoginManager);
+
+/* debugging prefs */
+//session_pref("browser.formfill.debug", false);
+//session_pref("browser.dom.window.dump.enabled", true);
+//session_pref("javascript.options.showInConsole", true);
+//session_pref("javascript.options.strict", true);
+//session_pref("nglayout.debug.disable_xul_cache", true);
+//session_pref("nglayout.debug.disable_xul_fastload", true);
+
+LoginManagerParent.init();
+
+add_hook("create_buffer_hook", function (buffer) {
+
+        buffer.browser.addEventListener("DOMFormHasPassword", function(event) {
+            content_buffer_dom_form_has_password_hook.run(buffer, event);
+        }, true /* captrue */);
+
+        buffer.browser.addEventListener("DOMAutoComplete", function(event) {
+            content_buffer_dom_auto_complete_hook.run(buffer, event);
+        }, true /* captrue */);
+
+        buffer.browser.addEventListener("blur", function(event) {
+            content_buffer_dom_auto_complete_hook.run(buffer, event);
+        }, true /* captrue */);
+
+        buffer.browser.addEventListener("change", function(event) {
+            content_buffer_dom_auto_complete_hook.run(buffer, event);
+        }, true /* captrue */);
+
+});
+
+add_hook("content_buffer_dom_form_has_password_hook", function(buffer, event) {
+    // Sometimes onFormPassword is undefined
+    if (LoginManagerContent.onFormPassword) {
+        LoginManagerContent.onFormPassword(event);
+    }
+});
+
+add_hook("content_buffer_dom_auto_complete_hook", function(buffer, event) {
+        LoginManagerContent.onUsernameInput(event);
+});
 
 // ========
 // Favicons
